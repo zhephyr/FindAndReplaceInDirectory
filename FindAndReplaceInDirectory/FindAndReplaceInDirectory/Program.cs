02fc22acda0f124f38d4be90a97cbbafa350749d
Michael Wang <wang.zhexi@gmail.com>
2018-05-07 13:34:47 -0500

    Syncing with work PC

--- FindAndReplaceInDirectory/FindAndReplaceInDirectory/Program.cs
+++ FindAndReplaceInDirectory/FindAndReplaceInDirectory/Program.cs
@@ -25,10 +25,9 @@ namespace FindAndReplaceInDirectory
     {
         static void Main(string[] args)
         {
-            var skuList = GetSkuData(@"C:\Users\Michael.wang\Downloads\FTP Files\DB Files\master-prices(old-new).csv");
-            var fileList = GetFileList(@"C:\Users\Michael.wang\Downloads\FTP Files\Views\all views");
+            var skuList = GetSkuData(@"C:\Users\Michael.wang\Downloads\FTP Files\DB Files\master-prices(old-new)(2).csv");
+            var fileList = GetFileList(@"C:\Users\Michael.wang\Downloads\FTP Files\Views\all views\product_partials\test");
             FindAndReplace(fileList, skuList);
-
         }
 
         static void FindAndReplace(List<string> fileList, List<SkuDatum> skuData)
@@ -38,10 +37,56 @@ namespace FindAndReplaceInDirectory
                 string text = File.ReadAllText(file);
                 foreach (var skuDatum in skuData)
                 {
-                    if (text.Contains(skuDatum.sku))
-                        File.WriteAllText(file, text.Replace(skuDatum.oldRetail, skuDatum.newRetail));
+                    if (text.Contains(skuDatum.sku) && text.Contains("26.00"))
+                    {
+                        text = ReplaceNearest(skuDatum, text);
+                    }
+
+                    File.WriteAllText(file, text);
                 }
             }
+        }
+
+        static string ReplaceNearest(SkuDatum sku, String fileText)
+        {
+            var lines = fileText.Split('\n');
+            var idx = 0;
+            var listZeros = new List<int>();
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("$26.00"))
+                {
+                    listZeros.Add(i);
+                }
+            }
+
+            if (listZeros.Count < 1)
+            {
+                return fileText;
+            }
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains(sku.sku))
+                {
+                    var nearIdx = 0;
+                    foreach (var zeroIdx in listZeros)
+                    {
+                        var a = Math.Abs(nearIdx - i);
+                        var b = Math.Abs(zeroIdx - i);
+                        if (nearIdx == 0 || (b < a))
+                        {
+                            nearIdx = zeroIdx;
+                        }
+                    }
+
+                    listZeros.Remove(nearIdx);
+                    lines[nearIdx] = lines[nearIdx].Replace("$26.00", "$" + sku.newRetail);
+                }
+            }
+
+            return String.Join("\n", lines);
         }
 
         static List<SkuDatum> GetSkuData(string filePath)
@@ -77,7 +122,8 @@ namespace FindAndReplaceInDirectory
             if (fileList.Any())
             {
                 var subDirs = Directory.GetDirectories(root);
-                foreach (var dir in subDirs) {
+                foreach (var dir in subDirs)
+                {
                     fileList.AddRange(GetFileList(dir));
                 }
             }
