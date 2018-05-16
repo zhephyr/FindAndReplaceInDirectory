7c3356de0b0ff24160ad89f0196f2a2d81ef2a84
zhephyr <wang.zhexi@gmail.com>
2018-05-16 15:38:20 -0500

    Split file info into a three item Tuple(# of tabs, element prefix, rest of text)

--- FindAndReplaceInDirectory/FindAndReplaceInDirectory/Program.cs
+++ FindAndReplaceInDirectory/FindAndReplaceInDirectory/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace FindAndReplaceInDirectory
 {
@@ -25,21 +24,27 @@ namespace FindAndReplaceInDirectory
     {
         static void Main(string[] args)
         {
-            var skuList = GetSkuData(@"C:\Users\Michael.wang\Downloads\FTP Files\DB Files\master-prices(old-new)(2).csv");
+            var skuList = GetSkuData(@"C:\Users\michaelw\DB FILES\master-prices(old-new)(2).csv");
             var fileList = GetFileList(@"C:\Users\Michael.wang\Downloads\FTP Files\Views\all views\product_partials\test");
             FindAndReplace(fileList, skuList);
         }
-
+		
+		// Takes a list of files and skus and does a ReplaceNearest() when sku is found.
         static void FindAndReplace(List<string> fileList, List<SkuDatum> skuData)
         {
             foreach (var file in fileList)
             {
                 string text = File.ReadAllText(file);
+				var lastChanged = 0.0;
                 foreach (var skuDatum in skuData)
                 {
                     if (text.Contains(skuDatum.sku) && text.Contains("26.00"))
                     {
-                        text = ReplaceNearest(skuDatum, text);
+						if (lastChanged == 0.0 || float.Parse(skuDatum.oldRetail) != lastChanged)
+						{
+							lastChanged = float.Parse(skuDatum.oldRetail);
+							text = ReplaceNearest(skuDatum, text);
+						}
                     }
 
                     File.WriteAllText(file, text);
@@ -47,49 +52,75 @@ namespace FindAndReplaceInDirectory
             }
         }
 
+		// Attempted functionality: Intelligently find and replace words in files by creating a branch stucture based on the HTML
+		// (or in this case, Jade/Pug) markup.
         static string ReplaceNearest(SkuDatum sku, String fileText)
         {
-            var lines = fileText.Split('\n');
+			var fileType = new MarkupType("Jade");
+			var splitFile = SplitFile(fileType, fileText);
+			var tabs = splitFile.Item1;
+			var prefixes = splitFile.Item2;
+			var lines = splitFile.Item3;
             var idx = 0;
             var listZeros = new List<int>();
 
-            for (var i = 0; i < lines.Length; i++)
-            {
-                if (lines[i].Contains("$26.00"))
-                {
-                    listZeros.Add(i);
-                }
-            }
+			Console.Write(tabs);
+			Console.ReadLine();
 
-            if (listZeros.Count < 1)
-            {
-                return fileText;
-            }
+			for (var i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].Contains("$26.00"))
+				{
+					listZeros.Add(i);
+				}
+			}
 
-            for (var i = 0; i < lines.Length; i++)
-            {
-                if (lines[i].Contains(sku.sku))
-                {
-                    var nearIdx = 0;
-                    foreach (var zeroIdx in listZeros)
-                    {
-                        var a = Math.Abs(nearIdx - i);
-                        var b = Math.Abs(zeroIdx - i);
-                        if (nearIdx == 0 || (b < a))
-                        {
-                            nearIdx = zeroIdx;
-                        }
-                    }
+			if (listZeros.Count < 1)
+			{
+				return fileText;
+			}
 
-                    listZeros.Remove(nearIdx);
-                    lines[nearIdx] = lines[nearIdx].Replace("$26.00", "$" + sku.newRetail);
-                }
-            }
+			//for (var i = 0; i < lines.Length; i++)
+			//{
+			//    if (lines[i].Contains(sku.sku))
+			//    {
+			//        var nearIdx = 0;
+			//        foreach (var zeroIdx in listZeros)
+			//        {
+			//            var a = Math.Abs(nearIdx - i);
+			//            var b = Math.Abs(zeroIdx - i);
+			//            if (nearIdx == 0 || (b < a))
+			//            {
+			//                nearIdx = zeroIdx;
+			//            }
+			//        }
 
-            return String.Join("\n", lines);
+			//        listZeros.Remove(nearIdx);
+			//        lines[nearIdx] = lines[nearIdx].Replace("$26.00", "$" + sku.newRetail);
+			//    }
+			//}
+
+			return String.Join("\n", lines);
         }
 
-        static List<SkuDatum> GetSkuData(string filePath)
+		private static Tuple<uint[], string[], string[]> SplitFile(MarkupType fileType, string fileText)
+		{
+			var lines = fileText.Split('\n');
+			var tabCnt = new uint[lines.Length];
+			var prefixes = new string[lines.Length];
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				tabCnt[i] = (uint)lines[i].Count(ch => ch == '\t');
+
+				var idx = lines[i].IndexOfAny(fileType.elemDelim);
+				prefixes[i] = lines[i].Trim().Substring(0, idx);
+			}
+
+			return new Tuple<uint[], string[], string[]>(tabCnt, prefixes, lines);
+		}
+
+		private static List<SkuDatum> GetSkuData(string filePath)
         {
             var skuList = new List<SkuDatum>();
 
@@ -106,7 +137,7 @@ namespace FindAndReplaceInDirectory
             return skuList;
         }
 
-        static List<string> GetFileList(string root)
+        private static List<string> GetFileList(string root)
         {
             var fileList = new List<string>();
 
---
+++ FindAndReplaceInDirectory/FindAndReplaceInDirectory/markupType.cs
@@ -0,0 +1,28 @@
+﻿// List of markup types and the prefix and delimiter strings that determine the seperate element names
+
+using System;
+
+namespace FindAndReplaceInDirectory
+{
+	class MarkupType
+	{
+		public string prefix;
+		public char[] elemDelim;
+		public MarkupType(string fileType)
+		{
+			switch(fileType)
+			{
+				case "Jade":
+					prefix = "";
+					elemDelim = new char[] { '.', '#', '(', ' ' };
+					break;
+				case "HTML":
+					prefix = "<";
+					elemDelim = new char[] { ' ' };
+					break;
+				default:
+					throw new NotImplementedException();
+			}
+		}
+	}
+}
